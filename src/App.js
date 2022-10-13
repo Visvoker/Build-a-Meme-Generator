fe553cf1d8f625b3f384eb4cc30ad1ad0d2585d0
visvoker <s2089533@gmail.com>
2022-10-13 11:28:27 +0800

    Add New

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Header from './component/Header'
 
 export default function App(){
   return(
-    <div>
+    <div className='HelloGit'>
       <Header />
+      <h1>Hello  </h1>
       <h1>Hello APP</h1>
+      <h1>New</h1>
+      <h1>New2</h1>
     </div>
   )
 }
